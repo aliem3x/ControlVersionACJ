6b89e66cab141d5a36c0b7f7e5b1b59e7276d770
Ali <Ali@AliEM>
2017-10-05 20:17:26 +0200

    Primer añadido

---
+++ P1.php
@@ -0,0 +1,27 @@
+<?php
+    $n=$_POST["num"];
+    echo "<h3>Tabla del $n</h3>";
+    $i=1;
+    echo"<table>";
+    do{
+            echo"<tr>";
+            echo "<td>$n x $i</td><td>".($n*$i)."</td>";
+            echo"</tr>";
+            $i++;
+     }while($i<=10);
+    echo"<table>";
+    echo '<style type="text/css">
+                    td{
+                            border-style: none;
+                            border: solid black;
+                            padding-right: .5em;
+                            padding-left: .5em;
+                            text-align: center;
+                    }
+    </style>';
+    echo'</br></br><a href="../index.html">Volver al MENÚ</a>';
+    
+    echo 'Primer Añadido';
+
+    
+?>
